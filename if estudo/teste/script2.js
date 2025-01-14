26c9a048bd2dcc64806e68613c701fc43e99e027
zehetekill <158592071+zehetekill@users.noreply.github.com>
2025-01-13 21:21:49 -0300

    Delete if estudo directory

--- if estudo/teste/script2.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('search-input').addEventListener('input', function () {
-    const query = this.value.toLowerCase();
-    const links = document.querySelectorAll('.links li');
-
-    links.forEach(link => {
-        const text = link.textContent.toLowerCase();
-        link.style.display = text.includes(query) ? 'block' : 'none';
-    });
-});
